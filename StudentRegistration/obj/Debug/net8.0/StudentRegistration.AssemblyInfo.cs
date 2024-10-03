e317938597e3b0c5024ca53924296e0c71d6d25f
gpespinosa <gpespinosa@gothongsoutherngroup.com>
2024-10-03 10:22:18 +0800

    directories and patches

--- StudentRegistration/obj/Debug/net8.0/StudentRegistration.AssemblyInfo.cs
+++ StudentRegistration/obj/Debug/net8.0/StudentRegistration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentRegistration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04b7f9c6b3bb70a63d557646dfe2df2b1d3db0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e385258224cbfae25561fee0aab21db36c510b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentRegistration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentRegistration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
